c5b173f1be12e526d6036a5a31a9f3a244207a14
AnthoG <anthony.dswg@gmail.com>
2024-01-16 18:10:23 -0500

    Few changes

--- src/pages/ProjectPortfolio.jsx
+++ src/pages/ProjectPortfolio.jsx
@@ -3,44 +3,48 @@
 import '../styles/Project.css';
 import { MarkGithubIcon } from '@primer/octicons-react';
 import Weather from '../assets/images/Weather.png';
+import TextEditorAnthoG from '../assets/images/TextEditorAnthoG.png'
+import DailyPlanner from '../assets/images/DailyPlanner.png'
+// import 
+
 
   var project = [
     {
        name: "WeatherDashboard",
        url: "https://anthogr45.github.io/WeatherDashboard/",
-       imagePath: "../src/assets/images/Weather.png",
+       imagePath: Weather,
        githublink: "https://github.com/anthogr45/WeatherDashboard"
     },
     {
       name: "PWA: Text Editor",
       url: "https://stormy-brushlands-38378-2a25ffa26458.herokuapp.com",
-      imagePath: "../src/assets/images/TextEditorAnthoG.png",
+      imagePath: TextEditorAnthoG,
       githublink: "https://github.com/anthogr45/-Progressive-Web-Applications-Text-Editor"
     },
     {
       name: "Find Your Movie (purple turtle)",
       url: "https://jizhang80.github.io/project1-purpleTurtle/",
-      imagePath: "../src/assets/images/Movie.jpg",
+      imagePath: DailyPlanner,
       githublink: "https://github.com/anthogr45/project1-purpleTurtle?tab=readme-ov-file"
     },
-    {
-      name: "Note Taker",
-      url: "https://floating-scrubland-52573-663844fb84ec.herokuapp.com/",
-      imagePath: "../src/assets/images/Notetaker.png",
-      githublink: "https://github.com/anthogr45/NoteTaker?tab=readme-ov-file"
-    },
-    {
-      name: "Dailyplanner",
-      url: "https://anthogr45.github.io/Dailyplanner/",
-      imagePath: "../src/assets/images/DailyPlanner.png",
-      githublink: "https://github.com/anthogr45/Dailyplanner"
-    },
-    {
-      name: "Antho-G: Tech-Blog",
-      url: "https://fierce-ridge-25865-7ffb2efa6e88.herokuapp.com",
-      imagePath: "../src/assets/images/Techblog.png",
-      githublink: "https://github.com/anthogr45/Tech-Blog?tab=readme-ov-file"
-    },
+    // {
+    //   name: "Note Taker",
+    //   url: "https://floating-scrubland-52573-663844fb84ec.herokuapp.com/",
+    //   imagePath: "../src/assets/images/Notetaker.png",
+    //   githublink: "https://github.com/anthogr45/NoteTaker?tab=readme-ov-file"
+    // },
+    // {
+    //   name: "Dailyplanner",
+    //   url: "https://anthogr45.github.io/Dailyplanner/",
+    //   imagePath: "../src/assets/images/DailyPlanner.png",
+    //   githublink: "https://github.com/anthogr45/Dailyplanner"
+    // },
+    // {
+    //   name: "Antho-G: Tech-Blog",
+    //   url: "https://fierce-ridge-25865-7ffb2efa6e88.herokuapp.com",
+    //   imagePath: "../src/assets/images/Techblog.png",
+    //   githublink: "https://github.com/anthogr45/Tech-Blog?tab=readme-ov-file"
+    // },
 
   ]
 
